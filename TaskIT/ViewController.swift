dfade3b9fe3e4744ba1822282744b9e7759084ee
legero <andrewcampbell@legeros-macbook-pro.local>
2018-07-15 08:10:35 -0500

    Initial Commit

---
+++ TaskIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskIT
+//
+//  Created by legero on 7/15/18.
+//  Copyright © 2018 legero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
