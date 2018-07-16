375aebc2998b99d3225c487cf5184ac130e55dd1
legero <andrewcampbell@legeros-MacBook-Pro.local>
2018-07-16 09:29:01 -0500

    add middle view with gradient

--- TaskIT/WelcomeController.swift
+++ TaskIT/WelcomeController.swift
@@ -10,18 +10,31 @@ import UIKit
 
 class WelcomeController: UIViewController {
 
+    let bg:UIView = {
+        let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .yellow
+        view.layer.cornerRadius = 6
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        view.addSubview(bg)
+        bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        bg.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive = true
+        bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
